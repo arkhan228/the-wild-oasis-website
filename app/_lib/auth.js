d04ba10503404824faf0773af3ef0ef51d430949
Abdul Rhman <ark573235@gmail.com>
2024-07-02 19:57:32 +0530

    reverted to initial commit

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -7,13 +7,6 @@ const authConfig = {
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
     }),
   ],
 
