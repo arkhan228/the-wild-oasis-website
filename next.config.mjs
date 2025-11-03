0d134c7fcbb57578ff75bf32589f55b319792099
Abdul Rhman <ark573235@gmail.com>
2025-11-03 13:56:16 +0530

    Added new Supabase project

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'parhuahzeiylrvfgxafx.supabase.co',
+        hostname: 'rjjjxqeprupwpctiuvui.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/cabin-images/**',
       },
